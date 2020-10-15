64f765b158ac79035217750a9be5a5bdf73785d4
Taha Elleuch <Tahaeleuch@gmail.com>
2020-10-15 23:40:17 +0100

    new ssl

--- web_front/CrossMe.py
+++ web_front/CrossMe.py
@@ -81,6 +81,6 @@ def signUp():
 
 
 if __name__ == "__main__":
-    app.secret_key = 'foued'
+    app.secret_key = 'mycrossme'
     """ Main Function """
     app.run(host='0.0.0.0', port=5000, ssl_context=('./ssl/server.crt', './ssl/server.key'))
