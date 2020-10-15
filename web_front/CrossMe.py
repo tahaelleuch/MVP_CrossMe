6be58d94f0d23fee5274d6307e48337322bb343b
fdetun <fouedads@gmail.com>
2020-10-15 18:00:21 +0100

    wdsd

--- web_front/CrossMe.py
+++ web_front/CrossMe.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 """ Starts a Flask CrossMe app """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json, url_for, redirect,flash
+from flask_login import LoginManager, UserMixin, login_required, login_user, current_user
 import uuid
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def crossme():
-    """CrossMe is live"""
+
+
+@app.route('/', strict_slashes=False, methods=['GET'])
+def home():
     return render_template('index.html', cache_id=uuid.uuid4())
+
+@app.route('/login',methods=['GET', 'POST'])
+def logg():
+    if request.method == 'POST':
+        """CrossMe is live"""
+        return "hello"
+    else:
+        return render_template('index.html', cache_id=uuid.uuid4())
+
+@app.route('/register',methods=['GET', 'POST'])
+def signUp():
+    if request.method == 'POST':
+        """CrossMe is live"""
+        return "register"
+    else:
+        return render_template('index.html', cache_id=uuid.uuid4())
+
+
 
 if __name__ == "__main__":
     """ Main Function """
--- web_front/static/scripts/index.js
+++ web_front/static/scripts/index.js
@@ -2,20 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
   $('.signin').click(function () {
     $('.myform').empty();
     $('.submit').empty();
+    $('.myform').attr('action', "/login");
     $('.myform').append(
     '<div class="mywhite"></div>' +
-    '<input type="email" id="email" placeholder="Email"><br>' +
-    '<input type="password" id="password" placeholder="PassWord"><br>'
+    '<input type="email" name="email" id="email" placeholder="Email"><br>' +
+    '<input type="password" name="password" id="password" placeholder="Password"><br>'
     )
     $('.submit').append(`Login`);
   });
   $('.signup').click(function () {
     $('.myform').empty();
     $('.submit').empty();
+    $('.myform').attr('action', "/register");
     $('.myform').append(
-    '<input type="text" id="full_name" placeholder="Your Full Name"><br>' +
-    '<input type="email" id="email" placeholder="Email"><br>' +
-    '<input type="password" id="password" placeholder="PassWord"><br>'
+    '<input type="text" name="full_name"  id="full_name" placeholder="Your Full Name"><br>' +
+    '<input type="email" name="email" id="email" placeholder="Email"><br>' +
+    '<input type="password" name="password" id="password" placeholder="PassWord"><br>'
     )
     $('.submit').append(`Sign Up`);
   });
