00116ef00471a57f284476fa1a7a4f13e958aea1
fdetun <fouedads@gmail.com>
2020-10-20 22:15:20 +0100

    search front unfollow + js

--- web_front/static/scripts/searchfollow.js
+++ web_front/static/scripts/searchfollow.js
@@ -8,8 +8,20 @@ let follow_code;
       type: 'POST',
       url: '/flw/' + follow_code,
       success: function ( response ) {
-        $('#'+follow_code).remove();
+        $('#'+follow_code).hide();
         $('#hi'+follow_code).show();
         }
       });
   });
+  $(document).on('click', '.follwed', function() {
+    a = document.getElementById("myuser_id").textContent;
+    follow_code = $(this).attr('alt');
+    $.ajax({
+      type: 'DELETE',
+      url: '/flw/' + follow_code,
+      success: function ( response ) {
+        $('#hi'+follow_code).hide();
+        $('#'+follow_code).show();
+        }
+      });
+      });
